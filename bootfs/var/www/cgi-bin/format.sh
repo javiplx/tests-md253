17f6ff051b5a07d0b4f669507798e01665f3319f
Javier Palacios <javiplx@gmail.com>
2013-12-26 22:02:42 +0100

    Create function to start/stop all optional services

--- bootfs/var/www/cgi-bin/format.sh
+++ bootfs/var/www/cgi-bin/format.sh
@@ -33,10 +33,7 @@ echo "hdd2 red clear" > /proc/mp_leds
 echo "hdd1 red set" > /proc/mp_leds
 echo "hdd2 red set" > /proc/mp_leds
 
-SERVICE="smb ftp btpd"
-for i in $SERVICE; do
- service_${i}_stop >/dev/null 2>&1
-done
+service_stop_all
 dlna_stop_daemon >/dev/null 2>&1 &
 $TwonkyMedia stop
 service_package_manager "Service&stop"
@@ -120,9 +117,7 @@ for i in $Directory; do
 done
 /bin/rm -rf /tmp/ftpaccess
 
-for i in $SERVICE; do
- service_${i}_start >/dev/null 2>&1 &
-done
+service_start_all
 dlna_start_daemon >/dev/null 2>&1 &
 $TwonkyMedia start
 
